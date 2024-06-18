3e951be0a443cc73439ea355fb06e2e3b27dfbab
Pulsar UA <sergivstep@gmail.com>
2024-06-18 23:14:55 +0300

    set delivery price

--- src/app/CalcCostDelivery.java
+++ src/app/CalcCostDelivery.java
@@ -7,7 +7,7 @@ package app;
 public class CalcCostDelivery extends CalcCostBase{
 
     // Вартість доставки
-    private final static double deliveryPrice ;
+    private final static double deliveryPrice = 7;
 
     // Розрахунок вартості товару,
     // з урахуванням вартості доставки
